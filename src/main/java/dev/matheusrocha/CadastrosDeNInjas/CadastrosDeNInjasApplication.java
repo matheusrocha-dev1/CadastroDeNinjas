54df74116dbddab3c356479b41d777c81b701054
Matheus Rodrigues <m.rocha.dev1@gmail.com>
2026-04-07 12:05:14 -0300

    first commit

---
+++ src/main/java/dev/matheusrocha/CadastrosDeNInjas/CadastrosDeNInjasApplication.java
@@ -0,0 +1,13 @@
+package dev.matheusrocha.CadastrosDeNInjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrosDeNInjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrosDeNInjasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/matheusrocha/CadastrosDeNInjas/CadastrosDeNInjasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.matheusrocha.CadastrosDeNInjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrosDeNInjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
