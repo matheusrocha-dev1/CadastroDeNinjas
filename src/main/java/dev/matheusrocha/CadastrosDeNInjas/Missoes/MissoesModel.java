5b91d858253558e3b1c88c5f17b80ee1f43990e4
Matheus Rodrigues <m.rocha.dev1@gmail.com>
2026-04-14 09:43:53 -0300

    feat: Coloca o Lombok na classe para abstracao

--- src/main/java/dev/matheusrocha/CadastrosDeNInjas/Missoes/MissoesModel.java
+++ src/main/java/dev/matheusrocha/CadastrosDeNInjas/Missoes/MissoesModel.java
@@ -2,17 +2,26 @@ package dev.matheusrocha.CadastrosDeNInjas.Missoes;
 
 import dev.matheusrocha.CadastrosDeNInjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private long id;
+
     private String nome;
+
     private String dificuldade;
 
     @OneToMany(mappedBy = "missoes")
--- src/main/java/dev/matheusrocha/CadastrosDeNInjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/matheusrocha/CadastrosDeNInjas/Ninjas/NinjaModel.java
@@ -1,12 +1,19 @@
 package dev.matheusrocha.CadastrosDeNInjas.Ninjas;
 import dev.matheusrocha.CadastrosDeNInjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.List;
 
 @Entity
 @Table(name = "tb_cadastro_de_ninjas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class NinjaModel {
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,44 +25,9 @@ public class NinjaModel {
 
     private int idade;
 
-
     @ManyToOne
     @JoinColumn(name = "missoes_id")
     private MissoesModel missoes;
 
 
-
-
-    public NinjaModel() {
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
 }
